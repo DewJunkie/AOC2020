705c864faefdd39a683ddc1ab9bc850085228be9
Duane McKinney <dmckinney@dabmi.com>
2021-03-24 05:02:24 -0500

    made samples included in the template optional

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,7 +5,7 @@ unsigned int Factorial(unsigned int number)
   return number <= 1 ? number : Factorial(number - 1) * number;
 }
 
-TEST_CASE("Factorials are computed", "[factorial]")
+TEST_CASE("Factorials are computed", "[factorial][sample]")
 {
   REQUIRE(Factorial(1) == 1);
   REQUIRE(Factorial(2) == 2);
